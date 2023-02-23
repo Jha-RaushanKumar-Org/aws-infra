8b0ed2afda653d1edff8a42f3428d095708d4ad0
Jha-RaushanKumar <jha.rau@northeastern.edu>
2023-02-23 05:19:02 -0500

    Assignment4 completed

--- modules/networking/network.tf
+++ modules/networking/network.tf
@@ -82,3 +82,74 @@ resource "aws_route" "public_route" {
   destination_cidr_block = var.destination_cidr_block
   gateway_id             = aws_internet_gateway.internet_gateway.id
 }
+
+# Create Security group
+resource "aws_security_group" "application_sg" {
+  name        = "application-sg"
+  description = "Security group for EC2 instance with web application"
+  vpc_id = aws_vpc.my_vpc.id
+  depends_on  = [aws_vpc.my_vpc]
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = "22"
+    to_port     = "22"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    protocol    = "tcp"
+    from_port   = "80"
+    to_port     = "80"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    protocol    = "tcp"
+    from_port   = "443"
+    to_port     = "443"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = var.DB_PORT
+    to_port     = var.DB_PORT
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    "Name" = "application-sg"
+  }
+}
+
+# Create EC2 instance
+
+resource "aws_instance" "ec2" {
+  ami                  = var.ami
+  instance_type        = var.instance_type
+  subnet_id            = aws_subnet.public_subnet[0].id
+  key_name             = var.key_name
+  security_groups      = [aws_security_group.application_sg.id]
+  disable_api_termination = false
+  ebs_block_device {
+    device_name           = "/dev/xvda"
+    volume_type           = var.instance_vol_type
+    volume_size           = var.instance_vol_size
+    delete_on_termination = true
+  }
+    user_data = <<EOF
+#!/bin/bash
+echo export DB_NAME=${var.DB_NAME} >> /etc/environment
+echo export DB_USER=${var.DB_USER} >> /etc/environment
+echo export DB_PASSWORD=${var.DB_PASSWORD} >> /etc/environment
+echo export DB_HOST="${var.DB_HOST} >> /etc/environment
+echo export DB_PORT=${var.DB_PORT} >> /etc/environment
+EOF
+tags = {
+    "Name" = "ec2"
+  }
+}
--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -25,3 +25,43 @@ variable "availability_zones" {
 variable "destination_cidr_block" {
   description = "Destination public cidr"
 }
+
+variable "DB_NAME" {
+  description = "DB name"
+}
+
+variable "DB_USER" {
+  description = "DB username"
+}
+
+variable "DB_PASSWORD" {
+  description = "DB password"
+}
+
+variable "DB_HOST" {
+  description = "DB host"
+}
+
+variable "DB_PORT" {
+  description = "DB port"
+}
+
+variable "ami" {
+  description = "AMI"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+}
+
+variable "instance_vol_type" {
+  description = "EC2 volume type"
+}
+
+variable "instance_vol_size" {
+  description = "EC2 volume size"
+}
+
+variable "key_name" {
+  description = "Name of ssh key"
+}
